c3776b31a8e85a9a866d42b3a0928dc3022f1dfb
ekaterina <kica111@mail.ru>
2022-08-25 23:04:39 +0800

    Решено 10 задание

---
+++ reshenie10/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру 
+//этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+System.Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+if(((number / 100) > 0) && ((number / 1000) < 1) )
+{
+    int secondNumber = (number/10) % 10;
+    System.Console.WriteLine( "Second number : "+ secondNumber);
+}
+
+else
+{
+    System.Console.WriteLine("Bad number");
+}
